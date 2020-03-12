2f5a924bb31fa251eed3a415a43e66da880bea69
Michael Mortenson (Warwick) <48418248+MJMortensonWarwick@users.noreply.github.com>
2020-03-12 10:25:20 +0000

    Add files via upload

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+import random
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+	# here we use 'render_template' to link to the html template
+	return render_template('home.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
